de86e0bfa14de53059465c0c8ae676cb3d8417aa
Francisca Amankrah <fiscaamankrah@gmail.com>
2025-07-22 15:22:48 +0000

    Add missing files and implement context refactor for userData

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,42 +1,17 @@
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import WelcomeMessage from './components/WelcomeMessage'
-import UserProfile from './components/UserProfile'
-
-import UserContext from './UserContext'
+import UserContext from './UserContext';
+import ProfilePage from './ProfilePage';
 
 function App() {
   const userData = {
-    name: "Alice",
-    age: "25",
-    bio: "Loves hiking and photography."
-  }
+    name: "Jane Doe",
+    email: "jane.doe@example.com"
+  };
 
   return (
     <UserContext.Provider value={userData}>
-      <>
-        <Header />
-        <MainContent />
-        <Footer />
-        <WelcomeMessage />
-        <UserProfile />
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-      </>
+      <ProfilePage />
     </UserContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
